f9864a284e47ad4463bedb0a18d2abf2c79f6f6f
Subham Panda <im.sspanda10@gmail.com>
2020-07-01 03:15:54 +0530

    feat: added routes, setup basic express app and server.js

---
+++ app.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+
+const userRouter = require('./routes/userRoutes');
+const viewRouter = require('./routes/viewRoutes');
+
+//Start express app
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('view', path.join(__dirname, 'views'));
+
+//GLOBAL MIDDLEWARES
+
+//Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Development logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+//ROUTES
+app.use('/', viewRouter);
+app.use('/api/users', userRouter);
+
+//Exporting the express app
+module.exports = app;
