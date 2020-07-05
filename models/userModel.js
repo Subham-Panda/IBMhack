7be76460f9fb00ad98b48cbcae29dee9f7e749a0
Subham Panda <im.sspanda10@gmail.com>
2020-07-05 15:21:28 +0530

    fix: Type to Boolean in user schema and speeling of seller

--- models/userModel.js
+++ models/userModel.js
@@ -1,32 +1,30 @@
-var mongoose = require("mongoose")
+const mongoose = require('mongoose');
 
-var UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     username: String,
     password: String,
     contactNo: Number,
     mail: String,
     about: String,
-    products: [
-        { type: mongoose.Schema.Types.ObjectId,
-            ref: "Product"}
-    ],
-    services: [ 
-        { type: mongoose.Schema.Types.ObjectId,
-            ref: "Service"}
-    ],
+    products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
+    services: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Service' }],
     ordersGiven: [
-        {isProduct: { type:bool, default:true },                                
-        product: {type: mongoose.Schema.Types.ObjectId, ref: "Product"},  
-        service: {type: mongoose.Schema.Types.ObjectId, ref: "Service"},
-        selller: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
-        isCompleted: { type:bool, default:false },}
+        {
+            isProduct: { type: Boolean, default: true },
+            product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+            service: { type: mongoose.Schema.Types.ObjectId, ref: 'Service' },
+            seller: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+            isCompleted: { type: Boolean, default: false },
+        },
     ],
     ordersReceived: [
-        {isProduct: { type:bool, default:true },
-        product: {type: mongoose.Schema.Types.ObjectId, ref: "Product"},  
-        service: {type: mongoose.Schema.Types.ObjectId, ref: "Service"},
-        buyer: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
-        isCompleted: { type:bool, default:false },}
+        {
+            isProduct: { type: Boolean, default: true },
+            product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+            service: { type: mongoose.Schema.Types.ObjectId, ref: 'Service' },
+            buyer: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+            isCompleted: { type: Boolean, default: false },
+        },
     ],
-})
-module.exports = mongoose.model("User",UserSchema);
+});
+module.exports = mongoose.model('User', UserSchema);
